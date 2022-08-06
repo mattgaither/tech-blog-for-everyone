9738b5d31247b8543ccdd3d726b5ed029a58e2c6
mattgaither <benjamin.gaither@shipt.com>
2022-08-06 10:56:59 -0500

    set up the server.js file

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const routes = require('./controllers/api');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening on port ' + PORT));
+});
